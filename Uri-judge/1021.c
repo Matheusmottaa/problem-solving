f542df02193b754e8c4b45b6594aec762c438f5b
Matheus Mota <matheusfelipe1918@gmail.com>
2021-12-06 18:25:37 -0300

    add more uri judge problems

--- Uri-judge/1021.c
+++ Uri-judge/1021.c
@@ -1,7 +1,5 @@
 #include <stdio.h> 
 #include <stdlib.h> 
-
-void otherSolution(); 
  
 int main() {
     double money, cedulas[] = {100.0, 50.0, 20.0, 10.0, 5.0, 2.0, 1.0, 0.5, 0.25, 0.10, 0.05, 0.01};
@@ -24,31 +22,4 @@ int main() {
         i++;
 	}
     return 0;
-}
-
-void otherSolution(){ 
-	int note, coins; 
-	double money;
-	scanf("%lf", &money); 
-	note = (int)money;
-	coins =  (money - note)*100;
-	printf("%d nota(s) de R$ 100.00\n", note/100);
-	note %= 100; 
-	printf("%d nota(s) de R$ 50.00\n", note/50);
-	note %=50; 
-	printf("%d nota(s) de R$ 20.00\n", note/20);
-	note %=20;
-	printf("%d nota(s) de R$ 10.00\n", note/10);
-	note %=10; 
-	printf("%d nota(s) de R$ 5.00\n", note/5);
-	note %=5; 
-	printf("%d nota(s) de R$ 2.00\n", note/2);
-	printf("MOEDAS: \n"); 
-	note %= 2; 
-	printf("%d moeda(s) de R$ 1.00\n", note);
-	printf("%d moeda(s) de R$ 0.50\n", coins/50); 
-	printf("%d moeda(s) de R$ 0.25\n", ((coins%50)/25));
-	printf("%d moeda(s) de R$ 10.00\n", ((coins%50)%25)/10);  
-	printf("%d moeda(s) de R$ 0.05\n", (((coins%50)%25)%10)/5);
-	printf("%d moeda(s) de R$ 0.001\n", (((coins%50)%25)%10)%5); 
 }
