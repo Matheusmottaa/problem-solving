7f0aba643b629325a7098221cefcdf569cab7efd
Matheus Mota <matheusfelipe1918@gmail.com>
2021-12-06 19:21:40 -0300

    add more uri judge problems

---
+++ Uri-judge/1037.c
@@ -0,0 +1,20 @@
+#include <stdio.h> 
+
+int main(){ 
+	float X; 
+	scanf("%f", &X); 
+	if(X<0 || X>100)
+		printf("Fora de intervalo\n"); 
+	else {
+		if(X<=25)
+			printf("Intervalo [0,25]\n");
+		else if(X<=50)
+			printf("Intervalo (25,50]\n");
+		else if(X<=75)
+			printf("Intervalo (50,75]\n"); 
+		else if(X<=100)
+			printf("Intervalo (75,100]\n");
+	}
+	return 0; 
+}
+
