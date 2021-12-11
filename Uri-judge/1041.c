6bd94522f2efec064f8789a2c98a045d2418a0c7
Matheus Mota <matheusfelipe1918@gmail.com>
2021-12-11 20:58:22 -0300

    add more uri judge problems

--- Uri-judge/1041.c
+++ Uri-judge/1041.c
@@ -3,31 +3,29 @@
 int main (){ 
 	float x, y; 
 	scanf("%f%f", &x, &y); 
-
 	if(x == 0 && y == 0)
 		printf("Origem\n"); 
 	if(x > 0){ 
-		if(y > 0)
-			printf("Q1\n"); 
-		if(y < 0)
-			printf("Q4\n"); 
-		if(y == 0 )
-			printf("Eixo X\n"); 
+	 if(y > 0)
+		printf("Q1\n"); 
+	 if(y < 0)
+		printf("Q4\n"); 
+	 if(y == 0 )
+		printf("Eixo X\n"); 
 	}
 	
 	if(x < 0){ 
-		if(y > 0)
-			printf("Q2\n"); 
-		if(y < 0 )
-			printf("Q3\n"); 
-		if(y == 0)
-			printf("Eixo X\n"); 
+	 if(y > 0)
+		printf("Q2\n"); 
+	 if(y < 0 )
+		printf("Q3\n"); 
+	 if(y == 0)
+		printf("Eixo X\n"); 
 	}
 	
 	if(x == 0)
-		if(y != 0 )
-			printf("Eixo Y\n"); 
-
+	 if(y != 0 )
+		printf("Eixo Y\n"); 
 	return 0; 
 } 
 
