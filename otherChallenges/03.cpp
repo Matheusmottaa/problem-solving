abce2635020e12690fe487334971f5b14a0e1f60
Matheus Mota <contact.mfmotta@gmail.com>
2022-09-18 12:50:34 -0300

    add more challegens

---
+++ otherChallenges/03.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std; 
+
+
+int main()
+{ 
+  int n; 
+  cin >> n; 
+  int vector[n]; 
+  
+  for(int i=0;i<n;i++)
+  { 
+   cin >> vector[i]; 
+  }
+
+  int aux = 0; 
+  int temp = 0;   
+  
+  for(int i=0;i<n;i++)
+  { 
+    if(i<n/2)
+    { 
+      temp = vector[n-(i+1)]; 
+      aux = vector[i]; 
+      vector[n-(i+1)] = aux; 
+      vector[i] = temp; 
+      vector[n-(i+1)] = aux; 
+	 }
+  }  
+  
+  for(int i=0;i<n;i++)
+  { 
+    cout << vector[i]; 
+    if(i<n-1)
+      cout << " "; 
+  }
+  
+  cout << endl; 
+
+  return 0; 
+}
