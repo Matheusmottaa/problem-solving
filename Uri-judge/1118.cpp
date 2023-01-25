b287f904572f5e4e54ccc9350aded8e80068320b
Matheus Mota <matheusfelipe1918@gmail.com>
2023-01-25 16:11:53 -0300

    Other solution to problem 1118.cpp

--- Uri-judge/1118.cpp
+++ Uri-judge/1118.cpp
@@ -1,43 +1,40 @@
 #include <iostream> 
+#include <iomanip> 
 
 using namespace std; 
 
-double average(double x, double y)
-{ 
-    return (x+y)/2.0; 
-}
-
 int main()
 { 
+    double score, sum = 0.0; 
+    int n = 1, count = 0; 
 
-    double score1, score2; 
-    int n = 0; 
-    cout.setf(ios_base::fixed, ios_base::floatfield); 
-    cout.precision(2); 
+    cout << fixed << setprecision(2); 
 
     while(n!=2)
     { 
-        do{ 
-            cin >> score1; 
-            if(score1<0||score1>10)
-                cout << "nota invalida\n"; 
-        }while(score1<0||score1>10); 
-        do{ 
-            cin >> score2; 
-            if(score2<0||score2>10)
-                cout << "nota invalida\n"; 
-        }while(score2<0 || score2 > 10); 
-
-        cout << "media = " << average(score1,score2) << endl;
-        cout <<  "novo calculo (1-sim 2-nao) ";  
-        cin >> n; 
-        if(n>2||n<1)
+        cin >> score; 
+        if(score>=0&&score<=10)
         { 
-            cout <<  "novo calculo (1-sim 2-nao) "; 
-            cin >> n; 
+            sum += score; 
+            count++; 
+
+            if(count == 2)
+            { 
+                cout << "media = " << sum/2.0 << endl; 
+                sum = 0.0; 
+                count = 0; 
+                cout << "novo calculo (1-sim 2-nao)\n"; 
+                cin >> n; 
+                while(n<1||n>2)
+                { 
+                    cout << "novo calculo (1-sim 2-nao)\n"; 
+                    cin >> n; 
+                }
+            }
+        }else { 
+            cout << "nota invalida\n"; 
         }
     }
-
     
 
     return 0; 
