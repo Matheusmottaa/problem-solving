28f4e26e0d058741a6a4dd55456fd2382ce87537
Matheus <contact.mfmotta@gmail.com>
2023-10-15 22:03:53 -0300

    Leetcode problems

---
+++ leetcode/Lt27.cpp
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/remove-element/
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int idx = 0; 
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(nums[i] != val)
+            {
+                nums[idx++] = nums[i]; 
+            }
+        }
+        return idx;
+    }
+};
